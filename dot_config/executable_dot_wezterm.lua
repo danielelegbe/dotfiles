bd0752a874ecfefe88ff92cbcdf79f63fc049d73
Daniel Elegbe <daniel_elegbe@hotmail.co.uk>
2024-10-28 11:42:33 +0000

    Add wezterm config

---
+++ dot_config/executable_dot_wezterm.lua
@@ -0,0 +1,37 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	default_cursor_style = "BlinkingBar",
+	font = wezterm.font("Monaspace Neon"),
+	font_size = 15.5,
+}
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Catppuccin Mocha"
+config.default_domain = "WSL:Ubuntu"
+
+config.keys = {
+	-- paste from the clipboard
+	{ key = "v", mods = "CTRL", action = act.PasteFrom("Clipboard") },
+
+	-- paste from the primary selection
+	{ key = "v", mods = "CTRL", action = act.PasteFrom("PrimarySelection") },
+	{
+		key = "n",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.ToggleFullScreen,
+	},
+}
+
+-- and finally, return the configuration to wezterm
+return config
