2f41338ec78767e9f02a5c7e61d83fff6af3f351
Daniel Elegbe <daniel_elegbe@hotmail.co.uk>
2024-11-05 22:27:27 +0000

    Additional config

--- dot_config/nvim/lua/plugins/harpoon.lua
+++ dot_config/nvim/lua/plugins/harpoon.lua
@@ -8,37 +8,9 @@ return {
 	config = function()
 		local harpoon = require("harpoon")
 		harpoon:setup({})
-		-- basic telescope configuration
-		local conf = require("telescope.config").values
-		local function toggle_telescope(harpoon_files)
-			local file_paths = {}
-			for _, item in ipairs(harpoon_files.items) do
-				table.insert(file_paths, item.value)
-			end
-
-			require("telescope.pickers")
-				.new({}, {
-					prompt_title = "Harpoon",
-					finder = require("telescope.finders").new_table({
-						results = file_paths,
-					}),
-					previewer = conf.file_previewer({}),
-					sorter = conf.generic_sorter({}),
-					initial_mode = "normal",
-				})
-				:find()
-		end
-
-		local map = vim.keymap.set
-		local opts = { noremap = true, silent = true, desc = "Open Harpoon" }
-		harpoon:setup()
-		map("n", "M", function()
-			harpoon:list():add()
-			vim.notify("󱡅  marked file")
-		end, opts)
 
 		vim.keymap.set("n", "H", function()
-			toggle_telescope(harpoon:list())
+			harpoon:list()
 		end, { desc = "Open harpoon window" })
 	end,
 }
--- dot_config/nvim/lua/plugins/neoscroll.lua
+++ dot_config/nvim/lua/plugins/neoscroll.lua
@@ -3,9 +3,6 @@ return {
 	config = function()
 		require("neoscroll").setup({
 			mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "zt", "zz", "zb" },
-			post_hook = function()
-				vim.cmd("normal! zz")
-			end,
 		})
 	end,
 }
