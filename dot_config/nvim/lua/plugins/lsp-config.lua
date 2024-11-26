40c42df8c5e0cc7fe21e2ff26cffcfb35c027b9d
Daniel Elegbe <danielelegbe@Daniels-MacBook-Pro.local>
2024-11-26 19:39:39 +0000

    Update lua lsp config

--- dot_config/nvim/lua/plugins/lsp-config.lua
+++ dot_config/nvim/lua/plugins/lsp-config.lua
@@ -139,6 +139,12 @@ return {
 			lua_ls = {
 				settings = {
 					Lua = {
+						runtime = { version = "LuaJIT" },
+						checkThirdParty = false,
+						library = {
+							"${3rd}/luv/library",
+							unpack(vim.api.nvim_get_runtime_file("", true)),
+						},
 						completion = {
 							callSnippet = "Replace",
 						},
