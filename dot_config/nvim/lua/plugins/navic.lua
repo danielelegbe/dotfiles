e97a9e8c7e44e0c57b0fcefee7467cd40cb67129
Daniel Elegbe <danielelegbe@Daniels-MacBook-Pro.local>
2024-12-30 21:29:49 +0000

    Add back breadcrumbs and navic

---
+++ dot_config/nvim/lua/plugins/navic.lua
@@ -0,0 +1,20 @@
+local icons = require("icons")
+local M = {
+	"SmiteshP/nvim-navic",
+}
+
+function M.config()
+	require("nvim-navic").setup({
+		icons = icons.kind,
+		highlight = true,
+		lsp = {
+			auto_attach = true,
+		},
+		click = true,
+		separator = " " .. icons.ui.ChevronRight .. " ",
+		depth_limit = 0,
+		depth_limit_indicator = "..",
+	})
+end
+
+return M
