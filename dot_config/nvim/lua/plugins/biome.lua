2e2155aba032b682a19621151414ff0fbd4c2956
Daniel Elegbe <daniel_elegbe@hotmail.co.uk>
2026-03-08 13:53:24 +0000

    Make PC the standard config

--- dot_config/nvim/lua/plugins/biome.lua
+++ dot_config/nvim/lua/plugins/biome.lua
@@ -1,33 +1,37 @@
 return {
-	-- Configure conform.nvim to use Biome for formatting
-	{
-		"stevearc/conform.nvim",
-		opts = {
-			formatters_by_ft = {
-				javascript = { "biome" },
-				typescript = { "biome" },
-				javascriptreact = { "biome" },
-				typescriptreact = { "biome" },
-				json = { "biome" },
-				jsonc = { "biome" },
-			},
-			formatters = {
-				biome = {
-					command = "bunx",
-					args = { "@biomejs/biome", "check", "--write", "--stdin-file-path", "$FILENAME" },
-					stdin = true,
-				},
-			},
-		},
-	},
+  -- Configure conform.nvim to use Biome for formatting
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        javascript = { "biome", "prettier" },
+        typescript = { "biome", "prettier" },
+        javascriptreact = { "biome", "prettier" },
+        typescriptreact = { "biome", "prettier" },
+        json = { "biome", "prettier" },
+        jsonc = { "biome", "prettier" },
+      },
+      formatters = {
+        biome = {
+          command = "bunx",
+          args = { "@biomejs/biome", "check", "--write", "--stdin-file-path", "$FILENAME" },
+          stdin = true,
+        },
+      },
+    },
+  },
 
-	-- Configure LSP to use Biome
-	{
-		"neovim/nvim-lspconfig",
-		opts = {
-			servers = {
-				biome = {},
-			},
-		},
-	},
+  -- Configure LSP to use Biome
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = {
+        enabled = false,
+      },
+      servers = {
+        biome = {},
+        eslint = {}
+      },
+    },
+  },
 }
